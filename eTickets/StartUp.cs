fdc1e09f973ad29a8f3c0625b97a94a8e51496e0
Ihor <mr.laptev.goga@mail.ru>
2022-03-28 21:06:45 +0300

    Init files

---
+++ eTickets/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+
+
+namespace eTickets
+{
+    public class StartUp
+    {
+        public IConfiguration Configuration { get; }
+        public StartUp(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello World");
+            });
+
+        }
+    }
+}
