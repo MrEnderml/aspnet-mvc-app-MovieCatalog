165c40344b4a429119a59b9f2f295157ddd80e1d
Ihor <mr.laptev.goga@mail.ru>
2022-04-03 14:39:40 +0300

    Account functionality

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
 ﻿using eTickets.Data;
+using eTickets.Data.Static;
+using eTickets.Data.ViewModels;
 using eTickets.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Controllers
 {
@@ -17,9 +20,92 @@ namespace eTickets.Controllers
             _signInManager = signInManager;
             _context = context;
         }
-        public IActionResult Index()
+
+        public async Task<IActionResult> Users()
+		{
+            var users = await _context.Users.ToListAsync();
+            return View(users);
+		}
+
+        public IActionResult Login()
         {
-            return View();
+            return View(new LoginVM());
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Movies");
+                    }
+                }
+                TempData["Error"] = "Wrong credentials. Please try again!";
+                return View(loginVM);
+            }
+
+            TempData["Error"] = "Wrong credentials. Please try again!";
+            return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            return View(new RegisterVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerVM);
+            }
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if(user != null)
+			{
+                TempData["Error"] = "This email address is already in use!";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+			if (newUserResponse.Succeeded)
+			{
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+			}
+            else
+			{
+                TempData["ValidError"] = newUserResponse.Errors.ToList()[0].Description;
+                return View(registerVM);
+			}
+
+            return View("RegisterCompleted");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+		{
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
+		}
     }
 }
